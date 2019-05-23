fa94421296007fe00da6ddc0230f00e5232fc718
ericsproles <tron04@gmail.com>
2019-05-22 23:10:53 -0700

    first commit

---
+++ final/client/apollo.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  client: {
+    name: 'graphql-fullstack-final',
+    service: 'graphql-fullstack-final',
+  },
+};
---
+++ start/client/src/resolvers.js
@@ -0,0 +1,32 @@
+import gql from 'graphql-tag';
+import { GET_CART_ITEMS } from '../src/pages/cart';
+
+export const typeDefs = gql`
+  extend type Query {
+    isLoggedIn: Boolean!
+    cartItems: [ID!]!
+  }
+
+  extend type Launch {
+    isInCart: Boolean!
+  }
+
+  extend type Mutation {
+    addOrRemoveFromCart(id: ID!): [Launch]
+  }
+`;
+
+export const schema = gql`
+  extend type Launch {
+    isInCart: Boolean!
+  }
+`;
+
+export const resolvers = {
+  Launch: {
+    isInCart: (launch, _, { cache }) => {
+      const { cartItems } = cache.readQuery({ query: GET_CART_ITEMS });
+      return cartItems.includes(launch.id);
+    },
+  },
+};
